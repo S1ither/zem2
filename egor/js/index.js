dae56691975de30be12edeb63a0e4d58183f99ed
s1ither <ophcik@mail.ru>
2021-03-16 18:10:25 +0500

    update links

--- egor/js/index.js
+++
@@ -1,7 +0,0 @@
-const articleBtnControlls = document.getElementById("articleControlls").querySelectorAll("button")
-
-const changeContentInPDF = (e) => document.getElementById("ViewPDF").setAttribute("src", `./docs/${e.target.innerText}.pdf`)
-
-for (const button of articleBtnControlls) {
-  button.addEventListener("click", changeContentInPDF)
-}
